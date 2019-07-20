e17c699d7f787640f30981ae716915bfc4a50191
Matt <matthew.wolff@yale.edu>
2019-07-20 14:52:16 -0400

    updated buttons / styles, spinning, "...."

--- js/script.js
+++ js/script.js
@@ -5,22 +5,10 @@ elem.onclick = function() {
   body.classList.toggle("invert");
 }
 
-
-// //
-// let loading = [".", "..", "...", "...."];
-// let i = -1;
-// setInterval(function() {
-//   if (i < loading.length - 1) {
-//     i++;
-//   }
-//   else {
-//     i = 0;
-//   }
-//   console.log(loading[i]);
-// }, 500)
+let dots = document.querySelector("#dots");
 
 let loading = [".", "..", "...", "...."];
 let i = 0;
 setInterval(function() {
-  console.log(loading[i++ % loading.length]);
+  dots.innerHTML = loading[i++ % loading.length];
 }, 500);
